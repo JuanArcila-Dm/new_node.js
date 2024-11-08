5f3c1d5fc2ee95fecc8d92dd54f112315bf99e85
JuanArcila-Dm <arcilajuandavid3@gmail.com>
2024-11-08 09:44:08 -0500

    node.js_New

---
+++ pages/product-catalog.js
@@ -0,0 +1,10 @@
+export default class ProductCatalog {
+    render() {
+      return `
+        <div class="product-catalog">
+          <h1>Product Catalog</h1>
+          <!-- Aquí irá el código HTML para mostrar los productos -->
+        </div>
+      `;
+    }
+  }
